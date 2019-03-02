0eed2ecf4425d273c9724b4d8a9e463201c79384
Benard Kong <benardkong@hotmail.com>
2019-03-02 21:02:43 +0900

    Created UT for accounts.test_models.UserModelTest & TokenModelTest

--- superlists/settings.py
+++ superlists/settings.py
@@ -45,7 +45,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'lists',
+    'accounts',
 ]
+
+AUTH_USER_MODEL = 'accounts.User'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
