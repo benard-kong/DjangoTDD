51cbfadc13260be8e48e0b6a1c5a10d8bc7e6deb
Benard Kong <benardkong@hotmail.com>
2019-03-21 06:56:51 +0900

    test for NewListForm, create_new helper method

--- lists/forms.py
+++ lists/forms.py
@@ -1,6 +1,7 @@
 from django import forms
-from lists.models import Item
+from lists.models import Item, List
 from django.core.exceptions import ValidationError
+from django.db import models
 
 EMPTY_ITEM_ERROR = "You can't have an empty list item"
 DUPLICATE_ITEM_ERROR = "You've already got this in your list"
@@ -23,8 +24,13 @@ class ItemForm(forms.models.ModelForm):
         self.instance.list = for_list
         return super().save()
 
-class NewListForm(object):
-    pass
+class NewListForm(ItemForm):
+
+    def save(self, owner):
+        if owner.is_authenticated:
+            List.create_new(first_item_text=self.cleaned_data['text'], owner=owner)
+        else:
+            List.create_new(first_item_text=self.cleaned_data['text'])
 
 class ExistingListItemForm(ItemForm):
     def __init__(self, for_list, *args, **kwargs):
--- lists/models.py
+++ lists/models.py
@@ -8,6 +8,9 @@ class List(models.Model):
     def get_absolute_url(self):
         return reverse('view_list', args=[self.id])
 
+    def create_new(self):
+        pass
+
 class Item(models.Model):
     text = models.TextField(default='')
     list = models.ForeignKey(List, default=None)
